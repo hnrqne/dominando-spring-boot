283c0a994388d7b7353debd37d980d1cb0d0dc89
Henrique Sampaio <hnrqne@gmail.com>
2024-03-21 00:52:01 -0300

    video05: explain get method, and RestController

---
+++ src/main/java/hnrqne/demo/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hnrqne.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("greetings")
+public class HelloController {
+
+    @GetMapping("hi")
+    public String hi(){
+        return "OMAE WA MO SHINDEIRU";
+    }
+}
