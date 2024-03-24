41956049d32af783a617e9d03a8d0b832c5ed312
Henrique Sampaio <hnrqne@gmail.com>
2024-03-24 18:48:51 -0300

    video09: PathVariable

--- src/main/java/hnrqne/demo/controller/CustomerController.java
+++ src/main/java/hnrqne/demo/controller/CustomerController.java
@@ -30,4 +30,11 @@ public class CustomerController {
     public List<String> filter(@RequestParam(name = "name") List<String> names){
         return NAMES.stream().filter(names::contains).toList();
     }
+
+    @GetMapping("{name}")
+    public String findByName(@PathVariable String name){
+        return NAMES.stream().filter(n -> n.equalsIgnoreCase(name))
+                .findFirst()
+                .orElseGet(() -> "");
+    }
 }
