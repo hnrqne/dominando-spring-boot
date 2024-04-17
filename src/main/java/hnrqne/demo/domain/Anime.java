97bc1b8c4ca20ed7d2181a08944572b5f540b055
Henrique Sampaio <hnrqne@gmail.com>
2024-04-17 00:07:39 -0300

    video20: exercise 05, DeleteMapping

--- src/main/java/hnrqne/demo/domain/Anime.java
+++ src/main/java/hnrqne/demo/domain/Anime.java
@@ -1,19 +1,18 @@
 package hnrqne.demo.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@ToString
 @Getter
 @Setter
-@AllArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Anime {
+    @EqualsAndHashCode.Include
     private Long id;
     @JsonProperty(value = "name")
     private String name;
@@ -21,14 +20,13 @@ public class Anime {
     private static List<Anime> animes = new ArrayList<>();
 
     static {
-        var jigokuraku = new Anime(1L, "Jigokuraku");
-        var konosuba = new Anime(2L, "Konosuba");
-        var drStone = new Anime(3L, "Dr.Stone");
+        var jigokuraku =  Anime.builder().id(1L).name("Jigokuraku").build();
+        var konosuba =  Anime.builder().id(2L).name("Konosuba").build();
+        var drStone =  Anime.builder().id(3L).name("Dr.Stone").build();
         animes.addAll(List.of(jigokuraku, konosuba, drStone));
     }
 
     public static List<Anime> getAnimes() {
         return animes;
     }
-
 }
